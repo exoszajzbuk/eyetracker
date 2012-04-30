84ef89fa8d8ad4ff05c7b174d79c4dd9425ea8fb
Balazs KOVACS <exoszajzbuk@gmail.com>
2012-05-01 00:50:05 +0200

    render method added

--- cvproba/main.cpp
+++ cvproba/main.cpp
@@ -8,6 +8,9 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <string>
+
+#include <QDebug>
 
 using namespace cv;
 using namespace std;
@@ -48,14 +51,167 @@ void drawDisplayPoint(Mat& p_image, Point p_point)
     line(p_image, Point(p_point.x, p_point.y-cross_size), Point(p_point.x, p_point.y+cross_size), yellow, 2);
 }
 
+Point calculatePosition(Point a, Point b, Point c, Point d, Point p, int screen_width, int screen_height, int calibPadding, double* relativePercentX, double* relativePercentY)
+{
+    double C = (double)(a.y - p.y) * (d.x - p.x) - (double)(a.x - p.x) * (d.y - p.y);
+    double B = (double)(a.y - p.y) * (c.x - d.x) + (double)(b.y - a.y) * (d.x - p.x) - (double)(a.x - p.x) * (c.y - d.y) - (double)(b.x - a.x) * (d.y - p.y);
+    double A = (double)(b.y - a.y) * (c.x - d.x) - (double)(b.x - a.x) * (c.y - d.y);
+
+    double D = B * B - 4 * A * C;
+
+    double u = (-B - sqrt(D)) / (2 * A);
+
+    double p1x = a.x + (b.x - a.x) * u;
+    double p2x = d.x + (c.x - d.x) * u;
+
+    double px = p.x;
+
+    double v = (px - p1x) / (p2x - p1x);
+
+    // in display bounds
+    /*
+    if (u > 1)
+        u = 1;
+    else if (u < 0)
+        u = 0;
+
+    if (v > 1)
+        v = 1;
+    else if (v < 0)
+        v = 0;
+    */
+
+    Point ret;
+    // calculate screen coordinates
+    ret.x = u*(screen_width-2*calibPadding)+calibPadding;
+    ret.y = v*(screen_height-2*calibPadding)+calibPadding;
+
+    // save relative coordinates
+    *relativePercentX = u;
+    *relativePercentY = v;
+
+    return ret;
+}
+
+int capture();
+int render(char **argv);
 
 int main(int argc, char** argv)
+{
+    string str1 ("r");
+
+
+    if (argv[1] == NULL)
+    {
+        capture();
+    }
+    else
+    {
+        string str2 (argv[1]);
+
+        if (str1.compare(str2) == 0)
+        {
+            qDebug("rendering");
+            render(argv);
+        }
+        else
+        {
+            qDebug("capturing");
+            capture();
+        }
+    }
+}
+
+
+int render(char** argv)
+{
+    stringstream filename;
+    filename << argv[2];
+
+    ifstream infile;
+    infile.open(filename.str().c_str());
+
+    int x,y;
+    char line[255];
+    int lines = 0;
+
+    vector<Point> points;
+
+    while (infile)
+    {
+        infile >> x;
+        infile >> y;
+
+        infile.getline(line, 255);
+
+        //qDebug("%d, %d", x, y);
+        lines++;
+
+        if (lines > 4)
+        {
+            Point p;
+            p.x = x;
+            p.y = y;
+
+            points.push_back(p);
+        }
+    }
+
+    infile.close();
+
+    int screen_width = 1280;
+    int screen_height = 1024;
+
+    Mat displayImage = Mat(screen_height, screen_width, CV_8UC3);
+
+    Mat testImage = imread("repin_FINAL.jpg");
+    resize(testImage, testImage, Size(), 2, 2);
+
+    Rect roi = Rect((displayImage.cols-testImage.cols)/2, (displayImage.rows-testImage.rows)/2, testImage.cols, testImage.rows);
+    Mat roiImg(displayImage, roi);
+    testImage.copyTo(roiImg);
+
+    namedWindow("Display", CV_WINDOW_AUTOSIZE | CV_GUI_NORMAL);
+    cvSetWindowProperty("Display", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+
+    for (unsigned int i = 0; i<points.size(); i++)
+    {
+        if (points[i].x < 0 || points[i].y < 0 || points[i].x > screen_width || points[i].y > screen_height)
+        {
+            points.erase(points.begin()+i);
+            i--;
+        }
+    }
+
+    for (unsigned int i = 0; i<points.size(); i++)
+    {
+        circle(displayImage, points[i], 2, Scalar(0,255,0), 2);
+
+        if (i < points.size())
+        {
+            cv::line(displayImage, points[i], points[i+1], Scalar(0, 255,255), 1, 8);
+        }
+    }
+
+    imshow("Display", displayImage);
+
+    int c;
+    do
+    {
+        c = waitKey(1);
+    }
+    while (c != 27);
+
+    return 0;
+}
+
+int capture()
 {
     VideoCapture cap;
 
     cap.open(1);
     if( cap.isOpened() )
-            cout << "Video " << argv[1] <<
+            cout << "Video " <<
                 ": width=" << cap.get(CV_CAP_PROP_FRAME_WIDTH) <<
                 ", height=" << cap.get(CV_CAP_PROP_FRAME_HEIGHT) <<
                 ", nframes=" << cap.get(CV_CAP_PROP_FRAME_COUNT) << endl;
@@ -78,7 +234,7 @@ int main(int argc, char** argv)
 
     // init frames
     Mat frame, gray, equalized, thresholded, inverted;
-    int thresh = 5;
+    int thresh = 8;
 
     // contours
     vector<vector<Point> > contours;
@@ -119,7 +275,7 @@ int main(int argc, char** argv)
     Vector<Point> calibPoints;
     Vector<Point> calibValues;
 
-    int calibPadding = 10;
+    int calibPadding = 0;
 
     calibPoints.push_back(Point(calibPadding, calibPadding));
     calibPoints.push_back(Point(screen_width-calibPadding, calibPadding));
@@ -130,12 +286,19 @@ int main(int argc, char** argv)
     int calibIndex = -1;
 
     Point screenPosition;
+    double relativePercentX;
+    double relativePercentY;
 
     // display
     Mat displayImage = Mat(screen_height, screen_width, CV_8UC3);
     bool displayOn = false;
 
+    Vector<Point> displayValues;
+
+    bool targetRendered = true;
+
     Mat testImage = imread("repin_FINAL.jpg");
+    resize(testImage, testImage, Size(), 2, 2);
 
     // init GUI
     namedWindow("Original", WINDOW_AUTOSIZE);
@@ -312,24 +475,13 @@ int main(int argc, char** argv)
             // calculate position
             Point p = eyePosition.center;
 
-            double C = (double)(a.y - p.y) * (d.x - p.x) - (double)(a.x - p.x) * (d.y - p.y);
-            double B = (double)(a.y - p.y) * (c.x - d.x) + (double)(b.y - a.y) * (d.x - p.x) - (double)(a.x - p.x) * (c.y - d.y) - (double)(b.x - a.x) * (d.y - p.y);
-            double A = (double)(b.y - a.y) * (c.x - d.x) - (double)(b.x - a.x) * (c.y - d.y);
+            screenPosition = calculatePosition(a, b, c, d, p, screen_width, screen_height, calibPadding, &relativePercentX, &relativePercentY);
 
-            double D = B * B - 4 * A * C;
-
-            double u = (-B - sqrt(D)) / (2 * A);
-
-            double p1x = a.x + (b.x - a.x) * u;
-            double p2x = d.x + (c.x - d.x) * u;
-
-            double px = p.x;
-
-            double v = (px - p1x) / (p2x - p1x);
-
-            // calculate screen coordinates
-            screenPosition.x = u*(screen_width-2*calibPadding)+calibPadding;
-            screenPosition.y = v*(screen_height-2*calibPadding)+calibPadding;
+            if (!targetRendered)
+            {
+                // save to display values
+                displayValues.push_back(screenPosition);
+            }
         }
 
         // display stuff on screen position
@@ -343,8 +495,11 @@ int main(int argc, char** argv)
             Mat roiImg(displayImage, roi);
             testImage.copyTo(roiImg);
 
-            // draw display point
-            drawDisplayPoint(displayImage, screenPosition);
+            if (targetRendered)
+            {
+                // draw display point
+                drawDisplayPoint(displayImage, screenPosition);
+            }
 
             // show image
             imshow("Display", displayImage);
@@ -370,6 +525,8 @@ int main(int argc, char** argv)
         }
 
         s << " | Screen position: (" << screenPosition.x << "," << screenPosition.y << ")";
+
+        s << " | Inside position: (" << relativePercentX << "," << relativePercentY << ")";
 
         displayStatusBar("Original", s.str(), 1000);
 
@@ -409,7 +566,7 @@ int main(int argc, char** argv)
                 imshow("Measurement", measureImage);
 
                 // destroy window if necessary
-                if (measureIndex == measurePoints.size())
+                if ((unsigned int)measureIndex == measurePoints.size())
                 {
                     destroyWindow("Measurement");
                 }
@@ -441,7 +598,7 @@ int main(int argc, char** argv)
             imshow("Calibration", calibImage);
 
             // destroy window if necessary
-            if (calibIndex == calibPoints.size())
+            if ((unsigned int)calibIndex == calibPoints.size())
             {
                 destroyWindow("Calibration");
                 calibIndex = -1;
@@ -465,18 +622,22 @@ int main(int argc, char** argv)
                 displayOn = false;
             }
         }
+        else if (c == 'p')
+        {
+            targetRendered = targetRendered?false:true;
+        }
     }
 
 
     // save if measurement if complete
-    if (measureIndex == measurePoints.size())
+    if ((unsigned int)measureIndex == measurePoints.size())
     {
         // set filename to current date
         time_t t = time(0);
         struct tm *now = localtime(&t);
 
         stringstream filename;
-        filename << (now->tm_year + 1900) << "-" << (now->tm_mon + 1) << "-" << now->tm_mday << "_" << now->tm_hour << "-" << now->tm_min << "-" << now->tm_sec << ".txt";
+        filename << "calib_" << (now->tm_year + 1900) << "-" << (now->tm_mon + 1) << "-" << now->tm_mday << "_" << now->tm_hour << "-" << now->tm_min << "-" << now->tm_sec << ".txt";
 
         ofstream outfile;
         outfile.open(filename.str().c_str());
@@ -493,8 +654,37 @@ int main(int argc, char** argv)
         }
 
         outfile.close();
-
     }
+
+    // save display data if present
+    if (displayValues.size() != 0)
+    {
+        // set filename to current date
+        time_t t = time(0);
+        struct tm *now = localtime(&t);
+
+        stringstream filename;
+        filename << "display_" << (now->tm_year + 1900) << "-" << (now->tm_mon + 1) << "-" << now->tm_mday << "_" << now->tm_hour << "-" << now->tm_min << "-" << now->tm_sec << ".txt";
+
+        ofstream outfile;
+        outfile.open(filename.str().c_str());
+
+        for (unsigned int i = 0; i<calibValues.size(); i++)
+        {
+            outfile << calibValues[i].x << " "
+                    << calibValues[i].y << " "
+                    << "calib" << endl;
+        }
+
+        for (unsigned int i = 0; i<displayValues.size(); i++)
+        {
+            outfile << displayValues[i].x << ";"
+                    << displayValues[i].y << endl;
+        }
+
+        outfile.close();
+    }
+
 
     return 0;
 }
