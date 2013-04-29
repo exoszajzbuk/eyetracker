529f07d3c6aa90c1fb83256bc4fa3b51ff69c372
Balazs KOVACS <exoszajzbuk@gmail.com>
2013-04-29 22:14:22 +0200

    sessions can be saved

--- eyetracker/session.cpp
+++ eyetracker/session.cpp
@@ -1,6 +1,9 @@
 #include "session.h"
 
+#include <iostream>
 #include <QDebug>
+#include <QDir>
+#include <QFile>
 
 Session::Session()
 {
@@ -25,8 +28,28 @@ void Session::addPoint(Point p)
 
 void Session::save(string name)
 {
-    qDebug(name.c_str());
-    qDebug() << points.size();
+    string folderName = string("session_")+name;
+    string imageName = folderName+"/image.png";
+    string pointsName = folderName+"/points.txt";
+
+    // create dir
+    QDir().mkdir(folderName.c_str());
+
+    // save image
+    QFile imageFile(imageName.c_str());
+    imageFile.open(QIODevice::WriteOnly);
+    image.save(&imageFile, "PNG");
+    imageFile.close();
+
+    // save points
+    QFile pointsFile(pointsName.c_str());
+    pointsFile.open(QIODevice::WriteOnly | QIODevice::Text);
+    QTextStream out(&pointsFile);
+
+    for (int i=0; i<points.size(); ++i)
+    {
+        out << points.at(i).x << "," << points.at(i).y << endl;
+    }
 }
 
 void Session::load(string name)
